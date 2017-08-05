1128b8006bd0d80a9d2edb371139720ad1bc68ba
kymkh0902 <kymkh0902@hanmail.net>
2017-08-05 14:01:04 +0900

    update

--- homework/Homework2_selenium_by흥_8_5.py
+++ homework/Homework2_selenium_by흥_8_5.py
@@ -9,6 +9,7 @@ from selenium import webdriver as wb
 from bs4 import BeautifulSoup as bs
 import datetime
 import re
+from naver_api import searchCompany 
 
 driver = wb.Chrome('C:/Users/Whi Kwon/Documents/GitHub/python_script/chromedriver.exe')
 driver.implicitly_wait(2)
@@ -20,7 +21,7 @@ today = today.strftime('%Y/%m/%d')
 week_ago = week_ago.strftime('%Y/%m/%d')
 week_day_ago = week_day_ago.strftime('%Y/%m/%d')
 
-pattern = re.compile('(대전|충청|충북|충남|세종)')
+pattern = re.compile(r'대전|충청|충북|충남|세종')
 
 #%%
 #조달청계약요청
@@ -31,7 +32,7 @@ driver.find_element_by_xpath('//*[@id="srchFrm"]/div[2]/div/a[1]/span').click()
 date_list = []
 price_list = []
 number_list = []
-region_list = []
+company_list = []
 
 pages = 3
 
@@ -45,21 +46,36 @@ for i in range(pages):
     price = [j.text.strip() for j in soup.find_all(attrs = {'class':'tr'}) if '원' in j.text]
     price = [int(i.replace('원','').replace(',','')) for i in price]  # string to int 변환
     number = [j.text.strip() for j in soup.find_all(name = 'a', attrs = {'href' : '#'}) if len(j.text) > 15]
-    # region
-    region = [j.text.strip() for j in soup.find_all(attrs = {'class':'tl'})]
-    region = region[1::2]
+    # company
+    company = [j.text.strip() for j in soup.find_all(attrs = {'class':'tl'})]
+    company = company[1::2]
     
+    # 매칭여부 확인 
+    NoMatching_add = []
+    NoMatching_nm = []
     # for문 대상은 date수를 대상으로.
     for j in range(len(date)):
         # 가격 2억 기준 if문
         if price[j] >= 2e8:
             # 패턴 매칭
-            if re.match(pattern, region[j]):
-                region_list.append(region[j])
+            if re.search(pattern, company[j]):
+                company_list.append(company[j])
             # 패턴 없을 경우, 내용 확인
             else:
                 driver.find_element_by_link_text(number[j]).click()
-                # 추가 기업 정보 확인 필요 
+                # 찾을 회사 이름 
+                company_nm = driver.find_element_by_xpath('//*[@id="container"]/div[4]/table[1]/tbody/tr[3]/td[1]').text.strip()
+                company_nm = company_nm.split('\n')[0].strip()
+                # 회사 주소 
+                _, company_add = searchCompany(company_nm)
+                
+                NoMatching_add.append(_)
+                NoMatching_nm.append(company_add)
+                # 조건 만족 여부 확인
+                for i in range(len(company_add)):
+                    if re.search(pattern, company_add[i]):  
+                        company_list.append(company_nm)
+                        break
                 # Nice 기업 정보 "https://www.nicebizinfo.com/ep/EP0100M001GE.nice?itgSrch=리젠코+정순희"
                 # 뒤로 가기
                 driver.back()
