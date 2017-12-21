df7e435548f722b8209e179be008347f62506e44
whikwon <whikwon@gmail.com>
2017-12-21 20:34:22 +0900

    week6 update, code feedback

--- deeplearning for all/Examples/week7/model.py
+++ deeplearning for all/Examples/week7/model.py
@@ -7,6 +7,23 @@
 ### 지금 문제점을 정확히 파악을 못하여, 정확히 못쓰는 점 양해 바람 ㄳㄳ
 
 
+
+
+
+## 전체적으로 코드가 문제가 많아서 다 고쳐주는 거 보다는 문제가 어디어디 있는지 알려줄테니 다시 짜보셈
+
+## 1. X, Y에 들어가는 데이터는 int 유형임. float 말고 int로 사용해야 함 (아무 것도 안 고치고 돌렸을 때 나오는 에러.)
+## 2. X_one_hot은 5개 class인 데이터 중에 0번 째면 [1, 0, 0, 0, 0] 으로 데이터를 변환해주는 코드임 (one-hot encoding)
+##    우리는 [batch_size, time_steps, features]로 데이터를 나타낼 건데 one_hot이 features를 만들어주게 됨.
+## 3. dataX, dataY 코드가 model에 있는데, 이 코드는 사용할 데이터를 만드는 코드이므로 data_util로 보낸 뒤에 train에서 읽는게 맞음.
+## 4. correction 구할 때 Y가 만약 [batch_size, seq_length]로 데이터가 들어오면 argmax를 씌울 필요가 없음. argmax는
+##    output 데이터가 [batch_size, time_steps, features]의 형식일 때 [batch_size, time_steps, 1]로 바꿔주기 위함임.
+## 5. train 함수가 없음. (기존 코드 참조.)
+## 6. eval에 X_one_hot을 feed_dict의 input으로 받는데 모델 내부적으로 X_one_hot은 처리하는 것이므로 X를 받아야 함.
+## 7. config에 데이터 관련 값들은 data_util로 가져간 뒤 train할 때 읽는게 나아보임.
+## 8. config에 중복된 변수나 이전 과제 변수들은 지울 필요가 있음.
+
+
 import tensorflow as tf
 
 class Model(object):
